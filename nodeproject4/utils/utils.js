2e1b26073e9a21a49385f5f75d1af54b1caaaf30
otr99x <otr99x@shaw.ca>
2017-09-07 16:12:10 -0600

    some stuff

--- nodeproject4/utils/utils.js
+++ nodeproject4/utils/utils.js
@@ -5,3 +5,10 @@ module.exports.add = (a,b) => {
 module.exports.square = (a) => {
   return a * a;
 };
+
+module.exports.setName = (user, fullname) => {
+  var names = fullname.split(' ');
+  user.firstname = names[0];
+  user.lastname = names[1];
+  return user;
+};
--- nodeproject4/utils/utils.test.js
+++ nodeproject4/utils/utils.test.js
@@ -1,10 +1,9 @@
 const utils = require('./utils');
+const expect = require('expect');
 
 it('should add 2 numbers', () => {
   var res = utils.add(2,3);
-  if(res !== 5){
-    throw new Error(`expected 5 but got ${res}`);
-  }
+  expect(res).toBe(5).toBeA('number');
 });
 
 it('should square 2 numbers', () => {
@@ -12,4 +11,50 @@ it('should square 2 numbers', () => {
   if(res !== 25){
     throw new Error(`expected 25 but got ${res}`);
   }
+});
+
+it('should be a Car', () => {
+  var acar = new Car();
+  expect(acar).toBeA(Car);
+});
+
+function Boat(){
+
+}
+
+function Car(){
+
+}
+
+// it('should expect some value', () => {
+//
+//   expect({
+//     name: "joe",
+//     age: 50,
+//   }).toEqual({
+//     name: "joe",
+//     age: 50,
+//   });
+//
+//   expect({
+//     name: "joe",
+//     age: 50,
+//   }).toBe({
+//     name: "joe",
+//     age: 50,
+//   });
+//
+//
+// });
+
+it('should verify first and last names are set', () => {
+  var user = utils.setName({
+    age: 15,
+    location: 'calgary',
+  }, 'Joe Cheung');
+
+  expect(user).toInclude({
+    firstname: 'Joe',
+    lastname: 'Cheung',
+  });
 });
