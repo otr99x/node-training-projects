544465c6a0930d50fa6685c00678866b3a306477
otr99x <otr99x@shaw.ca>
2017-09-08 10:44:18 -0600

    spy

---
+++ nodeproject4/spies/app.test.js
@@ -0,0 +1,31 @@
+const expect = require('expect');
+const rewire = require('rewire');
+
+var app = rewire('./app');
+describe('App', () => {
+  var db = {
+    saveUser: expect.createSpy(),
+  };
+  app.__set__('db',db);
+
+  it('should call saveUser with user object', () => {
+    var email = 'Joe@gmail.com';
+    var password = '12345';
+
+    app.handleSignup(email, password);
+    expect(db.saveUser).toHaveBeenCalledWith({
+      email: email,
+      password: password
+    });
+  });
+});
+
+describe('App demo', () => {
+  it('should call the spy correctly', () => {
+    var spy = expect.createSpy();
+    spy('Andrew',25);
+
+    expect(spy)
+    .toHaveBeenCalledWith('Andrew',25);
+  });
+});
