9e958204d5c68a70ed2081fdf75a5b32ec5e2df5
otr99x <otr99x@shaw.ca>
2017-09-14 13:25:28 -0600

    test

--- nodeproject5/server/server2.js
+++ nodeproject5/server/server2.js
@@ -13,6 +13,17 @@ var app = express();
 
 app.use(bodyParser.json());
 
+app.get('/todos', (req,res) => {
+  Todo.find().then((todos) => {
+    res.send({
+      todos: todos,
+      message: 'retrieved successfully',
+    });
+  }).catch((e) => {
+    res.status(404).send(e);
+  });
+});
+
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text,
@@ -51,6 +62,23 @@ app.get('/todos/:id', (req, res) => {
       }
     }).catch((e) => {
       res.status(400).send();
+    });
+  }
+});
+
+app.delete('/todos/:id', (req,res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    console.log('invalid id');
+    return res.status(404).send('invalid id');
+  }else{
+    Todo.findByIdAndRemove(id).then((todo) => {
+      res.send({
+         message: 'item removed',
+         item: todo,
+       });
+    }).catch((e) => {
+      res.status(404).send(e);
     });
   }
 });
